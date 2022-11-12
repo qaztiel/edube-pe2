a8f46ea2efe9a0015364ae04305a98d33be40f99
josephaorahim <josephaorahim@yahoo.com>
2022-11-12 16:00:25 -0700

    finished module 2

---
+++ module2.py
@@ -0,0 +1,17 @@
+list = [5,1,6,3,8,4,2,9,0]
+
+print(sorted(list))
+print(list)
+list.sort()
+print(list)
+
+
+"""Lab Is Palindrome"""
+def ispalindrome(s: str):
+    if s == s[::-1]:
+        print('its a palindrome')
+    else:
+        print('not a palindrome')\
+        
+ispalindrome('kayak')
+ispalindrome('joey')
