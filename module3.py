3ea997f2419664fc9221e46e4b068567e7f9058c
josephaorahim <josephaorahim@yahoo.com>
2022-11-12 23:52:29 -0700

    finished up module 3

--- module3.py
+++ module3.py
@@ -10,3 +10,23 @@ Exceptions as objects.
 
 """
 
+class Stack:
+    def __init__(self):
+        self.s = []
+
+
+    def push(self, num):
+        self.s.append(num)
+
+    def pop(self):
+        del self.s[-1]
+
+
+my_stack = Stack()
+
+my_stack.push(5)
+my_stack.push(6)
+my_stack.pop()
+print(my_stack.s)
+
+
